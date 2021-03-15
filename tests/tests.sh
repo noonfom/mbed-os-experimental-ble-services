a3cc58bde0c8d447e98ae01c73347a886dd011f8
Abbas Bracken Ziad <abbas.brackenziad@arm.com>
2021-03-15 13:17:20 +0000

    Add checks and documentation to tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
+usage() {
+  cat <<HELP_USAGE
+
+  $0 -s <service> -t <toolchain> -m <target>
+
+  -s Service test suite
+  -t Compile toolchain
+  -m Compile target
+HELP_USAGE
+}
+
 # Set pwd to script location
 cd "$( dirname "$0" )"
 
@@ -15,13 +26,47 @@ fi
 # Workaround: activate virtual environment here
 source ../activate.sh
 
-# Enter device folder of specified test suite
-cd TESTS/"$1"/device; shift
-
-# Build Mbed BLE app for all targets
-for target in "$@"
+# Parse options
+while getopts "s:t:m:h" opt
 do
-  mbed compile -t GCC_ARM -m "$target"
-  # TODO: support new tools
-  # mbed-tools compile -t GCC_ARM -m $target
+  case "$opt" in
+   s)
+     service="$OPTARG"
+     ;;
+   t)
+     toolchain="$OPTARG"
+     ;;
+   m)
+     target="$OPTARG"
+     ;;
+   h)
+     usage
+     exit 1
+     ;;
+  \?)
+    echo "Invalid option: -$OPTARG" 1>&2
+    exit 1
+    ;;
+  : )
+    echo "Invalid option: -$OPTARG requires an argument" 1>&2
+    exit 1
+    ;;
+  esac
 done
+
+# Verify that all arguments exist
+if [[ (-z "$toolchain") ||  (-z "$target") || (-z "$service") ]]
+then
+  echo "No arguments supplied." 1>&2
+  exit 1
+fi
+
+# Enter device folder for specified service
+cd TESTS/"$service"/device
+
+# Build Mbed BLE app
+mbed compile -t "$toolchain" -m "$target"
+# TODO: support new tools
+# mbed-tools compile -t GCC_ARM -m $target
+
+exit 0
